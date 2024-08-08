8fe38f2f27c8386d87ddf442c9be57003b278694
Parth Agrawal <pvagrawal0@gmail.com>
2024-08-08 15:04:13 -0400

    now am able to get blocked by entity maps - all files

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -10,8 +10,8 @@ function App() {
     <>
       <div>
         {/* <Main /> */}
-        {/* <Room /> */}
-        <MapCreator />
+        <Room />
+        {/* <MapCreator /> */}
       </div>
     </>
   );
